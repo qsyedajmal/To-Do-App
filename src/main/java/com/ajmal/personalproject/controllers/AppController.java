dc51ce0a4895ff9210078022bda3f3738963f8a1
Syed Ajmal <qsyedajmal@gmail.com>
2023-06-16 20:58:52 +0530

    To-Do-App

---
+++ src/main/java/com/ajmal/personalproject/controllers/AppController.java
@@ -0,0 +1,14 @@
+package com.ajmal.personalproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+	
+	@RequestMapping("/")
+	public String welcome() 
+	{
+		return "welcome";	
+	}   
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,23 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001,'ajmal', 'Learn AWS ', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002,'ajmal', 'Learn Azure ', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003,'ajmal', 'Get GCP Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004,'ajmal', 'Learn DevOps', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10005,'ajmal', 'Learn java', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10006,'ajmal', 'Get React Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10007,'ajmal', 'Learn Spring Boot', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10008,'ajmal', 'Learn DevOps', CURRENT_DATE(), false);
